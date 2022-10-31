1a782871d1446176d1a4f8425ddf5597d007c758
tom <mail>
2022-10-31 22:59:18 +0800

    nav 2

--- src/screens/SubNav/index.tsx
+++ src/screens/SubNav/index.tsx
@@ -1,6 +1,19 @@
 import "./index.css";
 const SubNav = () => {
-  return <header className="nav-2"></header>;
+  return (
+    <header className="nav-2">
+      <div className="container">
+        <ul>
+          <li>推荐</li>
+          <li>排行榜</li>
+          <li>歌单 </li>
+          <li>主播电台</li>
+          <li>歌手</li>
+          <li>新碟上架</li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default SubNav;
